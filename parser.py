e8bc0f6fce450682fbbedb1193991c3122104b90
Sergey Khaylov <Sergey.Haylov@gmail.com>
2015-03-06 01:02:14 +0300

    Add P. Add good field names to A parser

--- parser.py
+++ parser.py
@@ -22,44 +22,45 @@ class CommonParser(object):
             instances = list(instances)
 
         with open(filename, 'wb+') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            writer = csv.writer(csvfile, delimiter='|',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for instance in instances:
                 writer.writerow(instance.to_csv_row())
 
 
 class MasterParser(CommonParser):
-    column_c = None
-    column_d = None
-    column_e = None
-    column_f = None
-    column_g = None
-    column_h = None
-    column_i = None
-    column_j = None
+    date_file = None
+    crfn = None
+    recorded_borough = None
+    doc_type = None
+    document_date = None
+    document_amount = None
+    recorded_date = None
+    modified_date = None
 
     def process(self, row):
         self.unique_key = row[55:76]
-        self.column_c = row[17:27]
-        self.column_d = row[27:40]
-        self.column_e = row[40]
-        self.column_f = row[41:49]
-        self.column_g = row[49:59]
-        self.column_h = row[59:76].lstrip('0')
-        self.column_i = row[76:86]
+        self.date_file = row[17:27]
+        self.crfn = row[27:40]
+        self.recorded_borough = row[40]
+        self.doc_type = row[41:49]
+        self.document_date = row[49:59]
+        self.document_amount = row[59:76].lstrip('0')
+        self.recorded_date = row[76:86]
 
-        if self.column_h.replace('0', '').strip() == '.':
-            self.column_h = '$-'
+        if self.document_amount.replace('0', '').strip() == '.':
+            self.document_amount = '$-'
 
         row_copy = row[:]
         row_copy = row_copy.replace(' ', '')
         chunks = row_copy.split('/')[-3:]
-        self.column_j = '/'.join([chunks[0][-2:], chunks[1],chunks[2][:4]])
+        self.modified_date = '/'.join([chunks[0][-2:], chunks[1], chunks[2][:4]])
 
     def to_csv_row(self):
-        return [self.unique_key, 'A', self.column_c, self.column_d,
-                self.column_e, self.column_f, self.column_g, self.column_h,
-                self.column_i, self.column_j]
+        return [self.unique_key, 'A', self.date_file, self.crfn,
+                self.recorded_borough, self.doc_type, self.document_date,
+                self.document_amount, self.recorded_date,
+                self.modified_date]
 
 
 class PartyParser(CommonParser):
@@ -69,11 +70,57 @@ class PartyParser(CommonParser):
     address1 = None
     address2 = None
     country = None
+    city = None
     state = None
     zip_code = None
 
     def process(self, row):
+        import re
+
+        def clean_text(value):
+            value = value.strip()
+
+            if value.startswith('|'):
+                value = value[1:]
+
+            if value.endswith('|'):
+                value = value[:-1]
+
+            return value
+
         self.party_type = row[17]
+        items = [item.strip() for item in re.split('\s{3}', row)
+                 if item.strip()]
+
+        self.name = clean_text(items[0][18:])
+
+        if not items or len(items) < 3:
+            return
+
+        i = 1
+        self.address1 = clean_text(items[i])
+
+        if len(items) > 4:
+            i += 1
+            self.address2 = clean_text(items[i])
+        else:
+            self.address2 = ''
+
+
+        try:
+            i += 1
+            country_city = items[i]
+            self.country = clean_text(country_city[:2])
+            self.city = clean_text(country_city[2:])
+
+            i += 1
+            state_zip = items[i]
+            self.state = clean_text(state_zip[:-5])
+            self.zip_code = clean_text(state_zip[-5:])
+
+        except IndexError:
+            # import pdb;pdb.set_trace()
+            return
 
     def to_csv_row(self):
         return [self.unique_key, 'P', self.party_type, self.name,
